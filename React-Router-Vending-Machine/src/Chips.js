46201c55bb34d5a2c28918ecf5a76cd089207008
RandyFoster1016 <162823487+RandyFoster1016@users.noreply.github.com>
2024-08-01 14:15:31 -0400

    Add files via upload

---
+++ React-Router-Vending-Machine/src/Chips.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Chips = () => {
+    return (
+        <div>
+            <h2>Chips</h2>
+            <p>Chips are delicious snack made from thinnly sliced pototatoes, fried of baked until crispy. They come in various flavors.</p>
+            <Link to="/">Back to Vending Machine</Link>
+        </div>
+    );
+    }
+
+    export default Chips; 
